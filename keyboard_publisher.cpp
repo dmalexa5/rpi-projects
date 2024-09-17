934b87b00df3486f651981c291ecada30d0ec2eb
dmalexa5 <124112760+dmalexa5@users.noreply.github.com>
2024-09-17 07:58:14 -0400

    Update keyboard_publisher.cpp
    Modified the generic ros2 node from jazzy jalisco docs

--- keyboard_publisher.cpp
+++ keyboard_publisher.cpp
@@ -1,42 +1,43 @@
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
-#include <iostream>
+#include <chrono>
+#include <memory>
 #include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
 
 class KeyboardPublisher : public rclcpp::Node
 {
 public:
-    KeyboardPublisher()
-    : Node("keyboard_publisher")
-    {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("keyboard_data", 10);
-        timer_ = this->create_wall_timer(
-            100ms, [this]() { publish_keyboard_data(); });
-    }
-
-private:
-    void publish_keyboard_data()
-    {
+  KeyboardPublisher()
+  : Node("keyboard_publisher"), count_(0)
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("keyboard_data", 10);
+    auto timer_callback =
+      [this]() -> void {
         std::string input;
         std::getline(std::cin, input);
 
         auto message = std_msgs::msg::String();
-        message.data = input;
-        publisher_->publish(message);
+        message.data = std::to_string(this->count_++) + " >> " + input;
+          
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        this->publisher_->publish(message);
+      };
+    timer_ = this->create_wall_timer(100ms, timer_callback);
+  }
 
-        RCLCPP_INFO(this->get_logger(), "Published: '%s'", message.data.c_str());
-    }
-
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    rclcpp::TimerBase::SharedPtr timer_;
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<KeyboardPublisher>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<KeyboardPublisher>());
+  rclcpp::shutdown();
+  return 0;
 }

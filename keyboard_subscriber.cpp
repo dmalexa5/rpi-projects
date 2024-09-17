236c775deb4c4ad9b534c8b7913a9c1251da93e3
dmalexa5 <124112760+dmalexa5@users.noreply.github.com>
2024-09-17 08:01:19 -0400

    Update keyboard_subscriber.cpp

--- keyboard_subscriber.cpp
+++ keyboard_subscriber.cpp
@@ -1,27 +1,30 @@
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
-#include <iostream>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 class KeyboardSubscriber : public rclcpp::Node
 {
 public:
-    KeyboardSubscriber()
-    : Node("keyboard_subscriber")
-    {
-        subscription_ = this->create_subscription<std_msgs::msg::String>(
-            "keyboard_data", 10, [this](std_msgs::msg::String::UniquePtr msg) {
-                RCLCPP_INFO(this->get_logger(), "Received: '%s'", msg->data.c_str());
-            });
-    }
+  KeyboardSubscriber()
+  : Node("minimal_subscriber")
+  {
+    auto keyboard_data_callback =
+      [this](std_msgs::msg::String::UniquePtr msg) -> void {
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+      };
+    subscription_ =
+      this->create_subscription<std_msgs::msg::String>("keyboard_data", 10, keyboard_data_callback);
+  }
 
 private:
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<KeyboardSubscriber>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<KeyboardSubscriber>());
+  rclcpp::shutdown();
+  return 0;
 }

ee1285c6193ed2b8746fec734c325b95e810c67c
David Alexander <dmalexa5@ncsu.edu>
2024-09-24 22:04:36 -0400

    working gpio interface on python

--- servo_test.py
+++ servo_test.py
@@ -7,3 +7,18 @@
 # Currently, all I have to test is an HC-SR04 ultrasonic sensor mondule.
 # Need a stronger power source to run the servo motors.
 
+from gpiozero import DistanceSensor
+from time import sleep
+
+ultra = DistanceSensor(echo=27, trigger=17)
+
+try:
+	while True:
+		distance = ultra.distance * 100 # convert to cm
+		print(f"Distance: {distance:.2f} cm")
+		sleep(0.1)
+except KeyboardInterrupt:
+	print("Measurement stopped by user.")
+
+
+# IT'S WORKING YEAHHHHHHHHHHHHH BUDDY
